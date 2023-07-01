6e9f589eef4911b0ab0505afec3b6e49ef1444c3
Nicholas <nsant@vt.edu>
2023-07-01 10:35:36 -0400

    concat notes from multiple days

--- R/readFieldBookDB.R
+++ R/readFieldBookDB.R
@@ -12,9 +12,9 @@
 #' @details [fill in details here]
 #' @examples # none
 #' @export
-readFieldBookDB <- function(path, ontology = NULL, printTraitTable = FALSE, exportNA = FALSE, printDup = FALSE, exportFullFB = FALSE){
+readFieldBookDB <- function(path, ontology = NULL, printTraitTable = FALSE, exportNA = FALSE, printDup = FALSE, exportFullFB = FALSE, printDupPairs = TRUE, exportDupPairs = FALSE){
 	# c("plot_name", "trait", "value", "timestamp", "person", "location", "number")
-
+	# path = toCleanDir; printDup = TRUE; printTraitTable = FALSE; exportNA = FALSE; printDup = TRUE; exportFullFB = FALSE; printDupPairs = TRUE; exportDupPairs = FALSE
 	if(file.exists(path) & !dir.exists(path)){
 		fb <- read.csv(file)	
 	} else if(file.exists(path) & dir.exists(path)) {
@@ -33,9 +33,10 @@ readFieldBookDB <- function(path, ontology = NULL, printTraitTable = FALSE, expo
 		for(i in fbdb){
 			fbi <- read.csv(paste0(path, i))
 			fbi$file <- i
+			names(fbi)[names(fbi) == "plotName"] <- "plot_name"
 			fbL[[i]] <- fbi
 		}
-
+		# unique(lapply(fbL, names))
 		fb <- do.call(rbind, fbL)
 		row.names(fb) <- NULL
 
@@ -59,11 +60,16 @@ readFieldBookDB <- function(path, ontology = NULL, printTraitTable = FALSE, expo
 		# sum(dupRec)
 		# head(fb[dupRec,])
 		fb <- fb[!dupRec,]
-
+		fb$recordNo <- 1:nrow(fb)
 		isPic <- grepl("\\.jpg$", fb$value)
 		dupRecConf <- duplicated(fb[c("plot_name", "trait")]) & !isPic
 		fbdup <- fb[dupRecConf,]
-
+		if(printDupPairs){
+			fbdupPairs <- list()
+			for(i in 1:nrow(fbdup)){
+				fbdupPairs[[i]] <- fb[fb$plot_name %in% fbdup$plot_name[i] & fb$trait %in% fbdup$trait[i],]
+			}
+		}
 		# fb[fb$plot_name == "HRWPrelim_2022_WARVA_136",]		
 		# table(fb[dupRecConf,"trait"])
 		# head(fb[dupRecConf,], 20)
@@ -73,18 +79,37 @@ readFieldBookDB <- function(path, ontology = NULL, printTraitTable = FALSE, expo
 			print(fbdup)
 		} else {
 			message(paste0(nrow(fbdup), " records are duplicates with conflicting values, assigning value of last timestamp. Use 'printDup' to print duplicated records"))
-			if(printDup) print(fbdup)
+			if(printDup) {
+				if(printDupPairs) {
+					print(fbdupPairs)
+				} else {
+					print(fbdup) 
+				}
+			}
 		}
 
 		getLastRecord <- function(dupfbrec, returnRow = FALSE){
-			times <- as.POSIXlt(dupfbrec$timestamp)
+			times <- as.POSIXlt(dupfbrec$timestamp) # need to check value type then concatenate if notes, and pick later time point if not notes. 
 			keep <- which(times == max(times))
 			if(returnRow) return(dupfbrec[keep,]) else return(keep)
 		}
+		dropRecords <- NULL
 		for(i in 1:nrow(fbdup)){
-			dupRows <- which(fb$plot_name == fbdup$plot_name[i] & fb$trait == fbdup$trait[i])
-			fb <- fb[-dupRows[!dupRows %in% dupRows[getLastRecord(fb[dupRows, ])]],]
+			trait <- fbdup$trait[i]
+			dupRows <- which(fb$plot_name == fbdup$plot_name[i] & fb$trait == trait)
+			if(trait == "notes"){
+				fbd <- fb[dupRows, ]
+				allnotes <- paste0(gsub("\\s.*", "", fbd$timestamp), ": ", fb[dupRows, "value"])
+				fb[dupRows[1], "value"] <- paste(allnotes, collapse = "; ")
+				dropRecords <- c(dropRecords, dupRows[-1])
+			} else{
+				dropReci <- dupRows[!dupRows %in% dupRows[getLastRecord(fb[dupRows, ])]]
+				dropRecords <- c(dropRecords, dropReci)
+			}
+			# fb <- fb[-dupRows[!dupRows %in% dupRows[getLastRecord(fb[dupRows, ])]],]
 		}
+		fb[dropRecords,]
+		fb <- fb[-dropRecords, ]
 
 		if(any(duplicated(fb[c("plot_name", "trait")]) & !grepl("\\.jpg$", fb$value))) warning("there are still duplicates! This shouldnt happen.")
 		
@@ -96,6 +121,9 @@ readFieldBookDB <- function(path, ontology = NULL, printTraitTable = FALSE, expo
 	fb <- fb[order(trial, fb$trait, plotNos), ]
 	if(exportNA){
 		fb <- list(fb = fb, missing = exportNA)
+	}
+	if(exportDupPairs){
+		fb <- list(fb = fb, duplicateRecords = fbdupPairs)
 	}
 	return(fb)
 }
