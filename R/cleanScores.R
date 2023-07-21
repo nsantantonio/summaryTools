a16d6805f5c5a807d344e5ec5d7f53d176a5b20c
nsant <nsantantonio@gmail.com>
2023-07-21 15:31:02 -0400

    check for reps with no score data and do not impute. Need to check if working

--- R/cleanScores.R
+++ R/cleanScores.R
@@ -22,6 +22,18 @@ cleanScores <- function(fb, scoreTraits = NULL, zeroThreshold = 0.5){
 			return(FALSE)
 		}
 	}
+	checkRep <- function(fb, trait){
+		plotChr <- gsub(".*_", "", fb[["plot_name"]])
+		plotNo <- as.integer(plotChr)
+		if(all(1:max(plotNo) %in% plotNo)){ # THIS WILL BREAK IF YOU HAVE EXACTLY 99 ENTRIES IN REPLICATED TRIAL!
+			toZero <- is.na(fb[[trait]])
+		} else {
+			repl <- as.integer(substr(plotChr, 1, 1))
+			scoredRepl <- !tapply(fb[[trait]], repl, function(x) all(is.na(x)))
+			toZero <- repl %in% scoredRepl & is.na(fb[[trait]])
+		}
+		return(toZero)
+	}
 	if(is.null(scoreTraits)) {
 		scoreTraits <- names(fb)[sapply(fb, isScore)]
 		scoreTraits <- scoreTraits[!grepl("Bloc|Rep", scoreTraits, ignore.case = TRUE)]
@@ -29,13 +41,18 @@ cleanScores <- function(fb, scoreTraits = NULL, zeroThreshold = 0.5){
 		# if(length(scoreTraits) > 0) message("No score traits provided. The following traits were determined to be scores with values between 0 and 9.")
 		# print(scoreTraits)
 	}
+
+	# I NEED TO FIX THIS!!!! setting all NA to 0s, whern some reps not scored. 
 	if(length(scoreTraits)){
 		scoreCols <- grep(paste(scoreTraits, collapse = "|"), names(fb))
-		percMiss <- lapply(fb[scoreCols], function(x) sum(is.na(x)) / length(x))
+		percMiss <- sapply(fb[scoreCols], function(x) sum(is.na(x)) / length(x))
 		# needZeros <- names(fb)[scoreCols][percMiss > 0 & percMiss < 1]
 		needZeros <- names(fb)[scoreCols][percMiss >= zeroThreshold & percMiss < 1]
+		
 		for(i in needZeros){
-			fb[is.na(fb[[i]]), i] <- 0
+			# fb[is.na(fb[[i]]), i] <- 0
+			actuallyNeedZeros <- checkRep(fb, i)
+			if(any(actuallyNeedZeros)) fb[actuallyNeedZeros, i] <- 0
 		}
 		if(length(needZeros) > 0) {
 			message("The following traits were assumed to need scores of 0 imputed based on missingness:")
