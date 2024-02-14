c56f9a583822eda6fba2f8ec193b1aba80937a46
nsant <nsantantonio@gmail.com>
2024-02-14 06:45:22 -0500

    dont print NAs, empty cells instead

--- R/xtable2.R
+++ R/xtable2.R
@@ -12,13 +12,14 @@
 #' @param caption latex latex caption. If you complicated you might want to skip this and edit the output tex table 
 #' @param label latex latex label name
 #' @param size latex textsize. do not add slashes before, they will be appended automatically. 
+#' @param naReplace character. What character should replace missing table cells. Default is "". If NA is desired in the output, set naReplace = NULL
 #' @return [value]
 #' @details [fill in details here]
 #' @examples # none
 #' @export
-xtable2 <- function(tab, aln = NULL, pad = 0,  digits = 2, caption = NULL, label = NULL, size = "footnotesize", footNotes = NULL, include.rownames = TRUE, formatColNames = TRUE, statsLine = TRUE, specialChars = c("#"), ...){
+xtable2 <- function(tab, aln = NULL, pad = 0,  digits = 2, caption = NULL, label = NULL, size = "footnotesize", footNotes = NULL, include.rownames = TRUE, formatColNames = TRUE, statsLine = TRUE, specialChars = c("#"), naReplace = "", ...){
 	# if numeric, shoudl round to something
-	# tab <- prmat; pad = 0; digits = 2; aln = NULL; include.rownames = FALSE; formatColNames = TRUE; statsLine = TRUE
+	# tab <- usnCheckSum[c(1, 2:16)]; pad = 0; digits = 2; aln = NULL; include.rownames = FALSE; formatColNames = TRUE; statsLine = TRUE; size = "footnotesize"; specialChars = c("#"); caption = NULL; label = NULL; size = "footnotesize"; footNotes = NULL; naReplace = ""
 	size <- paste0("\\", size)
 
 	if(length(digits) == 1) {
@@ -49,6 +50,9 @@ xtable2 <- function(tab, aln = NULL, pad = 0,  digits = 2, caption = NULL, label
 		colTypes <- paste0(colTypes, collapse = "")
 	} else {
 		if(nchar(aln) != ncol(tab)) stop("nchar(aln) must equal the number of columns of tab!")
+	}
+	if(!is.null(naReplace)){
+		tab[is.na(tab)] <- naReplace
 	}
 	padTab <- matrix(NA, nrow(tab) + nrowColHead, ncol(tab))
 	padType <- strsplit(colTypes, "")[[1]]
